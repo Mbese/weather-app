0f08b15a9fbfe82dda21b581220096a4eb0c55ec
Lungelo Mbese <Lungelo.Mbese@standardbank.co.za>
2021-12-01 06:55:11 +0200

    Project commit

---
+++ app/src/main/java/com/android/weatherapp/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.android.weatherapp
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModel
+import com.android.weatherapp.service.WeatherRepository
+import com.android.weatherapp.service.dto.CurrentWeather
+import com.android.weatherapp.service.dto.FullWeather
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@SuppressLint("MissingPermission")
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    repository: WeatherRepository
+) : ViewModel() {
+    val current: Flow<CurrentWeather> = repository.getCurrentWeather()
+    val forecast: Flow<List<FullWeather.Daily>> = repository.getFiveDayForecast()
+}
